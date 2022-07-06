86ab286bfb714a131316054a25c5dd986349c1f2
MatoushAlpha <75540069+MatoushAlpha@users.noreply.github.com>
2022-07-06 21:26:58 +0000

    Tried to connect with Heroku and adjusted the web server settings to allow for it to run in production (hopefully, lol).

--- main.py
+++ main.py
@@ -11,4 +11,8 @@ app = Flask('app')
 def hello_world():
   return "Hello"
 
-app.run()
+if __name__ == "__main__":  # Makes sure this is the main process
+	app.run( # Starts the site
+		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
+		port=2000  # Randomly select the port the machine hosts on.
+	)
